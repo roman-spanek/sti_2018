6d481ae7d3b9dcc92ae0f1f02876ef8da24f74e2
roman-spanek <roman.spanek@tul.cz>
2018-05-15 08:41:50 +0200

    d

--- src/main/java/cz/roman/spanek/courses/hibernate/dao/SilniceDAO.java
+++ src/main/java/cz/roman/spanek/courses/hibernate/dao/SilniceDAO.java
@@ -2,10 +2,8 @@ package cz.roman.spanek.courses.hibernate.dao;
 
 import cz.roman.spanek.courses.hibernate.model.Silnice;
 
-public interface SilniceDAO {
-
-		Silnice findByKey(String key);
-    
+public interface SilniceDAO {  
+		Silnice findByKey(String key);      
 		void save(Silnice silnice);
     
 }
