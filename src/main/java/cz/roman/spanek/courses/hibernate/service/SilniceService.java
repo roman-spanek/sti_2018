4d1b06d4d72f081d501a1b72d82bacbb763e0a00
roman-spanek <roman.spanek@tul.cz>
2018-05-15 14:07:59 +0200

    fv

--- src/main/java/cz/roman/spanek/courses/hibernate/service/SilniceService.java
+++ src/main/java/cz/roman/spanek/courses/hibernate/service/SilniceService.java
@@ -2,6 +2,10 @@ package cz.roman.spanek.courses.hibernate.service;
 
 import cz.roman.spanek.courses.hibernate.model.Silnice;
 
+
 public interface SilniceService {	
 	Silnice findById(String id);
+  
+  
+  
 }
