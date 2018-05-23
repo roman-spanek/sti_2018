845ffd56aebe192b233df72ff71b1927cd5f5e07
roman-spanek <roman.spanek@tul.cz>
2018-05-23 10:23:42 +0200

    +2

--- src/main/java/cz/roman/spanek/courses/hibernate/HibernateCourseMainDriver.java
+++ src/main/java/cz/roman/spanek/courses/hibernate/HibernateCourseMainDriver.java
@@ -16,6 +16,8 @@ public class HibernateCourseMainDriver {
 	private static Logger logger = Logger.getLogger(HibernateCourseMainDriver.class);   
     static final String JDBC_DRIVER = "org.postgresql.Driver";    
     static final String DB_URL = "jdbc:postgresql://localhost:5432/";     
+    static final String JDBC_DRIVER = "org.postgresql.Driver";    
+    static final String DB_URL = "jdbc:postgresql://localhost:5432/";  
     static final String USER = "rdb_2018";         
     static final String PASS = "rdb_2018";         
     public static void main(String[] args) {   
