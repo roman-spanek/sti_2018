1712b49b1c556d4ad5aa61a6c44a41b16b5db1cf
roman-spanek <roman.spanek@tul.cz>
2018-06-05 09:27:29 +0200

    -6

--- src/main/java/cz/roman/spanek/courses/hibernate/HibernateCourseMainDriver.java
+++ src/main/java/cz/roman/spanek/courses/hibernate/HibernateCourseMainDriver.java
@@ -17,15 +17,7 @@ public class HibernateCourseMainDriver {
     static final String JDBC_DRIVER = "org.postgresql.Driver";    
     static final String DB_URL = "jdbc:postgresql://localhost:5432/";     
     static final String USER = "rdb_2018";         
-    static final String PASS = "rdb_2018";    
-  
-     static final String DB_URL = "jdbc:postgresql://localhost:5432/";     
-    static final String USER = "rdb_2018";         
-    static final String PASS = "rdb_2018";  
-     static final String DB_URL = "jdbc:postgresql://localhost:5432/";     
-    static final String USER = "rdb_2018";         
-    static final String PASS = "rdb_2018";     
-    
+    static final String PASS = "rdb_2018";        
        
     static final String PASS = "rdb_2018";                       
     public static void main(String[] args) {   
